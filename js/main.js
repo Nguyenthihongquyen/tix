4cdf16b023684c98f8fb080ca0811cb494971a2b
TNHao <trannhathao1516@gmail.com>
2021-06-15 23:14:30 +0700

    init for tix

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import 'https://code.jquery.com/jquery-3.5.1.slim.min.js'
+import 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js'
+import 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js'
